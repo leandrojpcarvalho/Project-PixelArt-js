f7122693c6c0728d503498e302051e224c169ab3
Leandro <ljpcarvalho@gmail.com>
2023-04-14 16:16:32 -0300

    quesito 4

--- script.js
+++ script.js
@@ -26,9 +26,35 @@ pathOfBody.lastChild.firstChild.appendChild(newTitle);
 // criação de cada paleta de cor
 const arrayColor = ['black', 'red', 'green', 'yellow'];
 for (let i = 0; i < 4; i += 1) {
-  newDiv = document.createElement('div');
-  newDiv.className = 'color';
-  newDiv.style.backgroundColor = arrayColor[i];
+    newDiv = document.createElement('div');
+    newDiv.className = 'color';
+    newDiv.style.backgroundColor = arrayColor[i];
 
-  pathOfBody.lastChild.lastChild.appendChild(newDiv);
+    pathOfBody.lastChild.lastChild.appendChild(newDiv);
 }
+
+const newButton = document.createElement('button');
+newButton.id = 'button-random-color';
+newButton.innerHTML = 'Cores aleatórias';
+
+pathOfBody.lastChild.lastChild.appendChild(newButton);
+
+
+function randomColors() {
+    const letters = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'];
+    let newColor = '#';
+
+    for (let i = 0; i < 6; i += 1) {
+        newColor += letters[Math.floor(Math.random() * 16)];
+    }
+    return newColor;
+}
+
+const pathOfButton = document.getElementById('button-random-color');
+const pathOfColor = document.getElementsByClassName('color');
+
+pathOfButton.addEventListener('click', function (event) {
+    for (let i = 0; i < 3; i += 1) {
+        pathOfColor[i + 1].style.backgroundColor = randomColors();
+    }
+});
