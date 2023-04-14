8da23ce02ac99ff060a7801e1b3f321a3fb770fd
Leandro <ljpcarvalho@gmail.com>
2023-04-14 15:32:11 -0300

    quesito 2

--- script.js
+++ script.js
@@ -2,8 +2,33 @@ const pathOfBody = document.getElementsByTagName('body')[0];
 
 //  criação do titulo
 
-const newTitle = document.createElement('h1');
+let newTitle = document.createElement('h1');
 newTitle.id = 'title';
 newTitle.innerText = 'Paleta de Cores';
 
 pathOfBody.appendChild(newTitle);
+
+// criação de uma seção para comportar a paleta de cores
+const newSection = document.createElement('section');
+
+pathOfBody.appendChild(newSection);
+
+let newDiv = document.createElement('div');
+newDiv.id = 'color-palette';
+
+pathOfBody.lastChild.appendChild(newDiv);
+
+//  criação do titulo h2
+newTitle = document.createElement('h2');
+newTitle.innerText = 'Paleta de Cores';
+pathOfBody.lastChild.firstChild.appendChild(newTitle);
+
+// criação de cada paleta de cor
+const arrayColor = ['blue', 'red', 'green', 'yellow'];
+for (let i = 0; i < 4; i += 1) {
+  newDiv = document.createElement('div');
+  newDiv.className = 'color';
+  newDiv.style.backgroundColor = arrayColor[i];
+
+  pathOfBody.lastChild.lastChild.appendChild(newDiv);
+}
